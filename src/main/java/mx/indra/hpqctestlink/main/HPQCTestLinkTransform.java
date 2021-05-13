753e9ad7af3e094dcde81a4cc00bfa2c486b6585
Gilberto <jgcolin@indracompany.com>
2021-05-13 12:09:22 -0500

    Se agrega git ignore

--- src/main/java/mx/indra/hpqctestlink/main/HPQCTestLinkTransform.java
+++ src/main/java/mx/indra/hpqctestlink/main/HPQCTestLinkTransform.java
@@ -6,7 +6,6 @@ import java.util.Iterator;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import mx.indra.hpqctestlink.beans.Actions;
 import mx.indra.hpqctestlink.beans.MTCP;
 import mx.indra.hpqctestlink.service.HPQCXLSProcessService;
 import mx.indra.hpqctestlink.service.ServiceInjector;
@@ -16,48 +15,39 @@ public class HPQCTestLinkTransform {
 	private static final Logger LOG = LoggerFactory.getLogger(HPQCTestLinkTransform.class);
 	private static final String EXCEL_FILE_LOCATION = "C:/Users/dalara/Desktop/files/PR558_MD13_D3480_PruebasdeSistema_Ciclo01.xlsx";
 
-	public static void main(String[] args){
-		
+	public static void main(String[] args) {
+
 		File excelFile = new File(EXCEL_FILE_LOCATION);
 		if (excelFile.exists()) {
-			
+
 			ServiceInjector serviceInjector = new ServiceInjector();
 			HPQCXLSProcessService hpqcxlsProcessService = null;
-			hpqcxlsProcessService =serviceInjector.getHPQCXLSProcessService();
+			hpqcxlsProcessService = serviceInjector.getHPQCXLSProcessService();
 			try {
-				
-				MTCP mtcp=hpqcxlsProcessService.processXLS(excelFile);
-				
+
+				MTCP mtcp = hpqcxlsProcessService.processXLS(excelFile);
+
 				LOG.info("LIST SIZE " + mtcp.getActions().size());
-				
-				for (int i = 0; i < mtcp.getActions().size() ; i++) {
-		            System.out.println("i: " + i);
-		            
-		            Iterator<String> it = mtcp.getActions().get(i).getActions().iterator();
-		            
-		            while (it.hasNext()) {
-		                System.out.println(it.next());
-		            }
-		       
-		        }
-				
-				
-				
+
+				for (int i = 0; i < mtcp.getActions().size(); i++) {
+					System.out.println("i: " + i);
+
+					Iterator<String> it = mtcp.getActions().get(i).getActions().iterator();
+
+					while (it.hasNext()) {
+						System.out.println(it.next());
+					}
+
+				}
+
 			} catch (Exception e) {
 				// TODO Auto-generated catch block
 				e.printStackTrace();
 			}
-			
-			
-			
-		}else {
+
+		} else {
 			LOG.info("Archivo no encontrado. Verificar que la ubicacion del archivo espeficifico sea el correcto");
 		}
-		
-		
-		
-	
-		
 
 	}
 }
